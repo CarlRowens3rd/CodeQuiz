ed9a37d4e58b3f627396b8b320839e2dd9811a1c
CarlRowens3rd <93492321+CarlRowens3rd@users.noreply.github.com>
2022-01-12 23:53:13 -0800

    Add files via upload

---
+++ CodeQuiz/resources/js/questions.js
@@ -0,0 +1,28 @@
+// questions *****************************************************
+var questions = [
+    {
+        title: "What is the main use for Javascript ",
+        choices: ["To enhance API's", "To create more dynamic webpages", " Used for the backend only ", "Use for front end only"],
+        answer: "To create more dynamic webpages"
+    },
+    {
+        title: "Which one of these isnt a data type",
+        choices: ["String", "Boolean", "float", "Scientic notation"],
+        answer: "Scientic notation"
+    },
+    {
+        title: "What is my SQL workbench used for",
+        choices: ["Creating databases", "creating webpages", "creating a new file in VS code", "To improve javascript functionality"],
+        answer: "Creating databases"
+    },
+    {
+        title: "what does title tag refer to in javascript",
+        choices: ["Title on tab bar in web browsers", "Creates comments", "linked javascript to HTML", "used as a class in HTML"],
+        answer: "Title of tab bar in web browsers"
+    },
+    {
+        title: "What tag do you use to link Css.style in HTML",
+        choices: ["<link>" , "<src>", "<h1>", "<meta>"],
+        answer: "link"
+    }
+]
